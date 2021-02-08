b50a21715119fd5a7e04f34ac61fd316ea4a88b0
Luciano Cardozo Casariego <lucianoc.c117@gmail.com>
2021-02-08 19:58:44 -0300

    Avanzando con mi primer app en react

--- src/App.js
+++ src/App.js
@@ -1,25 +1,75 @@
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
+import {tareas} from './tareas.json'
+
+
+class App extends React.Component{
+
+  constructor(){
+
+    super();
+
+    this.state = {
+      tareas
+    }
+  }
+
+  render(){
+
+    const tareas = this.state.tareas.map( (tarea , i) => {
+
+      return (
+
+        <div className="col-md-4 mt-4">
+          <div className="card">
+
+            <div className="card-header">
+              <h3>{tarea.titulo}</h3>
+              <span className="badge badge-pill badge-danger">
+                {tarea.prioridad}
+              </span>
+            </div>
+
+            <div className="card-body">
+              <p>{tarea.escripcion}</p>
+              <p><mark>{tarea.responsale}</mark></p>
+            </div>
+
+          </div>
+        </div>
+      )
+
+    })
+
+
+    return (
+      <div className="App">
+      
+        <nav className="navbar navbar-dark bg-dark">
+          <a href="" className="text-white ml-4">
+            
+              Tareas - 
+              
+              <span className="badge badge-pill badge-light">
+                {this.state.tareas.length}
+              </span>
+                          
+          </a>
+        </nav>
+
+        <div className="container">
+          <div className="row mt-4">
+            {tareas}
+          </div>          
+        </div>
+
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/componentes/FormularioTareas.js
@@ -0,0 +1,6 @@
+import React from "react";
+
+class FormularioTareas extends React.Component{
+
+    
+}
