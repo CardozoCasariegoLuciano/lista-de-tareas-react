d9a576b2efbbd436b114bb4adb923541be9d54be
luciano cardozo casariego <lucianoc.c117@gmail.com>
2021-02-23 21:51:32 -0300

    Terminado esta practica

--- src/App.js
+++ src/App.js
@@ -1,75 +1,97 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
-import {tareas} from './tareas.json'
+import { tareas } from "./tareas.json";
+import FormularioTareas from "./componentes/FormularioTareas";
 
-
-class App extends React.Component{
-
-  constructor(){
-
+class App extends React.Component {
+  constructor() {
     super();
 
     this.state = {
-      tareas
-    }
+      tareas,      
+    };
+
+    this.handleAddTarea = this.handleAddTarea.bind(this);
   }
 
-  render(){
+  handleAddTarea(tarea) {
+    this.setState({
+      tareas: [...this.state.tareas, tarea],
+    });
+  }
 
-    const tareas = this.state.tareas.map( (tarea , i) => {
+  handleDelete(index) {
+    this.setState({
+      tareas: this.state.tareas.filter((task, i) => {
+        return i !== index;
+      }),
+    });
+  }  
 
+  render() {
+    const tareas = this.state.tareas.map((tarea, i) => {
       return (
-
-        <div className="col-md-4 mt-4">
+        <div className="col-md-4 mt-4" key={i}>
           <div className="card">
-
             <div className="card-header">
               <h3>{tarea.titulo}</h3>
-              <span className="badge badge-pill badge-danger">
+              <span className="prioridad">
                 {tarea.prioridad}
               </span>
             </div>
 
             <div className="card-body">
               <p>{tarea.escripcion}</p>
-              <p><mark>{tarea.responsale}</mark></p>
+              <p>
+                <mark>{tarea.responsale}</mark>
+              </p>
             </div>
 
+            <div className="card-footer">
+              <button
+                className="btn btn-danger"
+                onClick={this.handleDelete.bind(this, i)}
+              >
+                Eliminar
+              </button>
+            </div>
           </div>
         </div>
-      )
-
-    })
-
+      );
+    });
 
     return (
       <div className="App">
-      
         <nav className="navbar navbar-dark bg-dark">
           <a href="" className="text-white ml-4">
-            
-              Tareas - 
-              
-              <span className="badge badge-pill badge-light">
-                {this.state.tareas.length}
-              </span>
-                          
+            Tareas -
+            <span className="badge badge-pill badge-light">
+              {this.state.tareas.length}
+            </span>
           </a>
         </nav>
 
         <div className="container">
           <div className="row mt-4">
-            {tareas}
-          </div>          
+            <div className="col-md-3">
+              <img src={logo} className="App-logo" alt="logo" />
+              <FormularioTareas onAgregarTarea={this.handleAddTarea} />
+            </div>
+
+            <div className="col-md-9">
+              <div className="row">{tareas}</div>
+            </div>
+          </div>
         </div>
-
-        <img src={logo} className="App-logo" alt="logo" />
-
       </div>
     );
   }
 }
 
 export default App;
+
+
+
+
--- src/componentes/FormularioTareas.js
+++ src/componentes/FormularioTareas.js
@@ -1,6 +1,90 @@
 import React from "react";
 
-class FormularioTareas extends React.Component{
+class FormularioTareas extends React.Component {
+  constructor() {
+    super();
 
+    this.state = {
+      titulo: "",
+      responsale: "",
+      escripcion: "",
+      prioridad: "",
+    };
+
+    this.handleInput = this.handleInput.bind(this)
+    this.handleSubmit = this.handleSubmit.bind(this)
+  }
+
+  handleInput(e) {
+    const { value, name } = e.target;
+
+    this.setState({
+        [name] : value
+    });
+  }
+
+  handleSubmit(e){
+
+    e.preventDefault()    
+    this.props.onAgregarTarea(this.state)
     
+    console.log("Enviando la informacion")
+  }
+
+  render() {
+    return (
+      <div className="card">
+        <form className="card-body" onSubmit={this.handleSubmit}>
+          <div className="form-group my-1">
+            <input
+              type="text"
+              name="titulo"
+              onChange={this.handleInput}
+              placeholder="Title"
+              className="form-control"
+            />
+          </div>
+
+          <div className="form-group my-1">
+            <input
+              type="text"
+              name="responsale"
+              onChange={this.handleInput}
+              placeholder="Responsalbe"
+              className="form-control"
+            />
+          </div>
+
+          <div className="form-group my-1">
+            <input
+              type="text"
+              name="escripcion"
+              onChange={this.handleInput}
+              placeholder="Descripcion"
+              className="form-control"
+            />
+          </div>
+
+          <div className="form-group my-1">
+            <select
+              name="prioridad"
+              className="form-control"
+              onChange={this.handleInput}
+            >              
+              <option>low</option>
+              <option>medium</option>
+              <option>high</option>
+            </select>
+          </div>
+
+          <button type="submit" className="btn btn-primary">
+            {" "}
+            Guardar
+          </button>
+        </form>
+      </div>
+    );
+  }
 }
+
+export default FormularioTareas;
